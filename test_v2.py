22bc17f228fc445249119b5397baddea396bd0ad
olafishappy <41749660+nincrw@users.noreply.github.com>
2021-04-21 02:52:17 +0700

    Update test_v2.py

--- test_v2.py
+++ test_v2.py
@@ -5,3 +5,16 @@ st.sidebar.title('Traffic')
 
 st.markdown('Interactive Dashboard 🔥')
 st.sidebar.markdown('Traffic 🔥')
+
+file_name = 'Tweets.csv'
+
+# Read CSV
+@st.cache(persist=True)
+def read_data():
+	data = pd.read_csv(data_path + '\\' + file_name)
+	data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+	return data
+
+data = read_data()
+
+st.write(data)
