fc7be41427e67adc1bbd952df34244e6a3109d36
olafishappy <41749660+nincrw@users.noreply.github.com>
2021-04-22 23:37:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly as px
-# import plotly.express as px
+import plotly.express as px
 
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
